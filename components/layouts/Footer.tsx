b21bd114339b920d369a8bc433218e2a8445b857
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2025-01-08 22:29:01 +0700

    slicing footer

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -3,7 +3,17 @@ import { NextPage } from "next";
 interface Props {}
 
 const Footer: NextPage<Props> = ({}) => {
-  return <div></div>;
+  return (
+    <footer className="bg-black py-8 text-center text-white">
+      <div className="container mx-auto">
+        Copyright &copy; 2025 by{" "}
+        <a target="_blank" href="" className="text-violet-500 font-semibold">
+          Ivan Adi Saputra
+        </a>
+        . All rights reserved
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
